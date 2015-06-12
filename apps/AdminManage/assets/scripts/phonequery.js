85157cb19fa640f7a43cdf1779b808ec09341d69
darrenshuai <gingershuai@163.com>
2015-06-12 18:04:33 +0800

    update

---
+++ apps/AdminManage/assets/scripts/phonequery.js
@@ -0,0 +1,221 @@
+var oTable;
+// 用户操作的选项
+
+$(document).ready(function() {
+	// 默认差1个礼拜内的信息
+	$("#startTime").val(getNowTime(-7));
+	$("#endTime").val(getNowTime(0));
+	// $("#searchItems").validationEngine();
+	// 获取消息通知
+	// getPhoneRecords();
+	// $("#search").checkPermission();
+	getPhoneRecords();
+
+	App.init();
+	audiojs.events.ready(function() {
+		var as = audiojs.createAll();
+	});
+
+	$(".form_datetime").datetimepicker({
+		isRTL: App.isRTL(),
+		language: 'zh-CN',
+		format: "yyyy-mm-dd hh:ii",
+		autoclose: true,
+		pickerPosition: (App.isRTL() ? "bottom-right" : "bottom-left")
+	});
+
+});
+
+// 搜索功能
+$("#search").click(function() {
+	if (oTable == undefined) {
+		getPhoneRecords();
+	} else {
+		oTable.fnDraw();
+	}
+});
+
+
+function getPhoneRecords() {
+	oTable = $('#datatable').dataTable({
+		"bAutoWidth": false, // 不自动计算列宽度
+		"bProcessing": true, // 加载数据时显示正在加载信息
+		"bServerSide": true, // 指定从服务器端获取数据
+		"bFilter": false, // 使用过滤功能
+		"sAjaxSource": "http://115.29.102.106/juzi/web/phoneRecord", // 获取数据的url
+		"fnServerData": getData, // 获取数据的处理函数
+		"sPaginationType": "bootstrap", // 翻页界面类型
+		"bSort": false, // 不启用排序
+		"bLengthChange": true, // 改变每页显示数据数量
+		"iDisplayLength": 25, // 每页显示的数据行数
+		"aLengthMenu": [
+			[25, 50, 100],
+			[25, 50, 100]
+		],
+		"fnServerParams": function(aoData) {
+			var charge = [];
+			var userType = [];
+			$.each($("input[name=charge]:checked"), function(i,
+				o) {
+				charge.push($(o).val());
+			});
+			$.each($("input[name=userType]:checked"), function(
+				i, o) {
+				userType.push($(o).val());
+			});
+			aoData.push({
+				"name": "userIds",
+				"value": $("#userIds").val()
+			});
+			aoData.push({
+				"name": "minDuration",
+				"value": $("#minDuration").val()
+			});
+			aoData.push({
+				"name": "maxDuration",
+				"value": $("#maxDuration").val()
+			});
+			aoData.push({
+				"name": "startTime",
+				"value": $("#startTime").val()
+			});
+			aoData.push({
+				"name": "endTime",
+				"value": $("#endTime").val()
+			});
+			aoData.push({
+				"name": "charge",
+				"value": charge
+			});
+			aoData.push({
+				"name": "userType",
+				"value": userType
+			});
+
+		},
+		"aoColumns": [ // 列设置，表有几列，数组就有几项
+			{
+				"mDataProp": "phoneRecordId"
+			}, {
+				"mDataProp": "fromUserId"
+			}, {
+				"mDataProp": "toUserId"
+			}, {
+				"mDataProp": "startTime"
+			}, {
+				"mDataProp": "endTime"
+			}, {
+				"mDataProp": "duration"
+			}, {
+				"mDataProp": null,
+				"fnRender": function(obj) {
+					// 操作按钮
+					return obj.aData.chargeRate == 0 ? "免费" : obj.aData.chargeRate + "元/分钟";
+				}
+			}, {
+				"mDataProp": null,
+				"fnRender": function(obj) {
+					// 操作按钮
+					return '<audio src="http://kolber.github.io/audiojs/demos/mp3/juicy.mp3" preload="auto"></audio>'
+				}
+			}
+		],
+		"aoColumnDefs": [{
+			sDefaultContent: '',
+			aTargets: ['_all']
+		}],
+		"oLanguage": { // 汉化
+			"sLengthMenu": "每页显示 _MENU_ 条记录",
+			"sZeroRecords": "没有检索到数据",
+			"sInfo": "当前数据为从第 _START_ 到第 _END_ 条数据；总共有 _TOTAL_ 条记录",
+			"sInfoEmtpy": "没有数据",
+			//"sProcessing": '<img src="img/ajax-loaders/ajax-loader-7.gif" title="img/ajax-loaders/ajax-loader-7.gif">',
+			"oPaginate": {
+				"sFirst": "首页",
+				"sPrevious": "上一页",
+				"sNext": "下一页",
+				"sLast": "尾页"
+			}
+		}
+	});
+
+	function getData(sSource, aoData, fnCallback) {
+		$.ajax({
+				url: sSource,
+				type: 'get',
+				dataType: 'jsonp',
+				jsonp: '_jsonp',
+				data: {
+					aoData: JSON.stringify(aoData)
+				}
+			})
+			.done(function(data, status, xhr) {
+				var errCode = data.err_code;
+				if (errCode == undefined) {
+					fnCallback(data);
+				} else {
+					alert(data.err_msg);
+					if (errCode == "12000") {
+						location.href = "login.html";
+					}
+				}
+			})
+			.fail(function() {})
+			.always(function() {
+				console.log("complete");
+			});
+	}
+
+}
+
+function getNowTime(i, isDay) {
+	var nowDate = new Date();
+
+	if (i != 0) {
+		nowDate.setDate(nowDate.getDate() + i);
+	}
+	if (isDay) {
+		nowDate.setHours(0);
+		nowDate.setMinutes(0);
+		nowDate.setSeconds(0, 0);
+	}
+	return nowDate.dateFormat("yyyy-MM-dd HH:mm");
+}
+
+Date.prototype.dateFormat = function(fmt) {
+	var o = {
+		"M+": this.getMonth() + 1, // 月份
+		"d+": this.getDate(), // 日
+		"h+": this.getHours() % 12 == 0 ? 12 : this.getHours() % 12, // 小时
+		"H+": this.getHours(), // 小时
+		"m+": this.getMinutes(), // 分
+		"s+": this.getSeconds(), // 秒
+		"q+": Math.floor((this.getMonth() + 3) / 3), // 季度
+		"S": this.getMilliseconds()
+			// 毫秒
+	};
+	var week = {
+		"0": "\u65e5",
+		"1": "\u4e00",
+		"2": "\u4e8c",
+		"3": "\u4e09",
+		"4": "\u56db",
+		"5": "\u4e94",
+		"6": "\u516d"
+	};
+	if (/(y+)/.test(fmt)) {
+		fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "")
+			.substr(4 - RegExp.$1.length));
+	}
+	if (/(E+)/.test(fmt)) {
+		fmt = fmt
+			.replace(
+				RegExp.$1, ((RegExp.$1.length > 1) ? (RegExp.$1.length > 2 ? "\u661f\u671f" : "\u5468") : "") + week[this.getDay() + ""]);
+	}
+	for (var k in o) {
+		if (new RegExp("(" + k + ")").test(fmt)) {
+			fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+		}
+	}
+	return fmt;
+};
---
+++ apps/AdminManage/assets/scripts/userquery.js
@@ -0,0 +1,253 @@
+var statusArr = ["正常", "待审核", "封号", "低质量", "封禁设备", "封号一天"],
+	OdataTable;
+var DataManage = function() {
+	return {
+		init: function() {
+
+			if (!jQuery().dataTable) {
+				return;
+			};
+			OdataTable = $('#userdata').dataTable({
+				"sAjaxSource": "http://115.29.102.106/juzi/web/users/list", // 获取数据的url
+				"bFilter": false, // 使用过滤功能
+				"bAutoWidth": false,
+				"bServerSide": true, // 指定从服务器端获取数据
+				"fnServerData": getData, // 获取数据的处理函数
+				"iDisplayLength": 25,
+				"aLengthMenu": [
+					[25, 50, 100],
+					[25, 50, 100]
+				],
+				"fnServerParams": function(aoData) {
+					var userStatus = [];
+					$.each($("input[name=userStatusCheck]:checked"),
+						function(i, o) {
+							userStatus.push($(o).val());
+						});
+					aoData.push({
+						"name": "minUserId",
+						"value": $("#minUserId").val()
+					});
+					aoData.push({
+						"name": "maxUserId",
+						"value": $("#maxUserId").val()
+					});
+					aoData.push({
+						"name": "gender",
+						"value": $("#gender").val()
+					});
+					aoData.push({
+						"name": "userName",
+						"value": $("#username").val()
+					});
+					aoData.push({
+						"name": "lastOnlineTimeFrom",
+						"value": $("#lastonlinetimeFrom").val()
+					});
+					aoData.push({
+						"name": "lastOnlineTimeTo",
+						"value": $("#lastonlinetimeTo").val()
+					});
+					aoData.push({
+						"name": "minReportCount",
+						"value": $("#minReportCount").val()
+					});
+					aoData.push({
+						"name": "maxReportCount",
+						"value": $("#maxReportCount").val()
+					});
+					aoData.push({
+						"name": "accurateUserIds",
+						"value": $("#accurateUserIds").val()
+					});
+					aoData.push({
+						"name": "userSearchType",
+						"value": $("input[name=userIdRadio]:checked")
+							.val()
+					});
+					aoData.push({
+						"name": "userStatus",
+						"value": userStatus
+					});
+					aoData.push({
+						"name": "minChargeRate",
+						"value": $("#minChargeRate").val()
+					});
+					aoData.push({
+						"name": "maxChargeRate",
+						"value": $("#maxChargeRate").val()
+					});
+					aoData.push({
+						"name": "minTotalDuration",
+						"value": $("#minTotalDuration").val()
+					});
+					aoData.push({
+						"name": "maxTotalDuration",
+						"value": $("#maxTotalDuration").val()
+					});
+					aoData.push({
+						"name": "description",
+						"value": $("#description").val()
+					});
+					console.log(aoData);
+				},
+				"aoColumns": [{
+					"mDataProp": "userId"
+				}, {
+					"mDataProp": null,
+					"fnRender": function(obj) {
+						if (obj.aData.gender == 0) {
+							return '未知';
+						} else if (obj.aData.gender == 1) {
+							return '男';
+						} else {
+							return '女';
+						}
+					}
+				}, {
+					"mDataProp": "nickName"
+				}, {
+					"mDataProp": null,
+					"fnRender": function(obj) {
+						var len = obj.aData.userImages.length;
+						var images = "";
+						if (len > 0) {
+							for (var i = 0; i < len; i++) {
+								images += '<a href="' + obj.aData.userImages[i].normal + '" target="_blank"> <img src="' + obj.aData.userImages[i].normal + '" style="width: 100px; " /> </a>';
+							}
+							return images;
+						} else {
+							return "暂无图片"
+						}
+
+					}
+				}, {
+					"sClass": "left",
+					"mDataProp": "description"
+				}, {
+					"mDataProp": null,
+					"fnRender": function(obj) {
+						return '语音长度';
+					}
+				}, {
+					"mDataProp": "chargeRate"
+				}, {
+					"mDataProp": "totalDuration"
+				}, {
+					"mDataProp": "reportCount"
+				}, {
+					"mDataProp": null,
+					"fnRender": function(obj) {
+						var tag = obj.aData.tag;
+						if (null == tag) {
+							tag = "无";
+						}
+						return tag;
+					}
+				}, {
+					"mDataProp": "balance"
+				}, {
+					"mDataProp": "actualbalance"
+				}, {
+					"mDataProp": null,
+					"fnRender": function(obj) {
+						return statusArr[obj.aData.status];
+					}
+				}, {
+					"mDataProp": null,
+					"fnRender": function(obj) {
+						return "录音src";
+					}
+				}, {
+					"mDataProp": null,
+					"fnRender": function(obj) {
+						// 操作按钮
+						return '<a class="btn blue">修改状态</a>'
+					}
+				}],
+				"aoColumnDefs": [{
+					sDefaultContent: '',
+					aTargets: ['_all']
+				}],
+				"oLanguage": { // 汉化
+					"sLengthMenu": "每页显示 _MENU_ 条记录",
+					"sZeroRecords": "没有检索到数据",
+					"sInfo": "当前数据为从第 _START_ 到第 _END_ 条数据；总共有 _TOTAL_ 条记录",
+					"sInfoEmtpy": "没有数据",
+					"sProcessing": '<img src="img/ajax-loaders/ajax-loader-7.gif" title="img/ajax-loaders/ajax-loader-7.gif">',
+					"oPaginate": {
+						"sFirst": "首页",
+						"sPrevious": "上一页",
+						"sNext": "下一页",
+						"sLast": "尾页"
+					}
+				}
+			});
+
+			function getData(sSource, aoData, fnCallback) {
+				$.ajax({
+						url: sSource,
+						type: 'get',
+						dataType: 'jsonp',
+						jsonp: '_jsonp',
+						data: {
+							aoData: JSON.stringify(aoData)
+						},
+
+					})
+					.done(function(data, status, xhr) {
+						var errCode = data.err_code;
+						if (errCode == undefined) {
+							fnCallback(data);
+						} else {
+							alert(data.err_msg);
+							if (errCode == "12000") {
+								location.href = "login.html";
+							}
+						}
+					})
+					.fail(function() {})
+					.always(function() {
+						console.log("complete");
+					});
+			}
+		}
+	};
+
+}();
+
+$(function() {
+	App.init();
+	$(".form_datetime").datetimepicker({
+		isRTL: App.isRTL(),
+		language: 'zh-CN',
+		format: "yyyy-mm-dd hh:ii",
+		autoclose: true,
+		pickerPosition: (App.isRTL() ? "bottom-right" : "bottom-left")
+	});
+
+	$('label.more').on('click', function() {
+		$('.more-filter').slideToggle();
+	});
+
+	//切换radio
+	$('input[name="userIdRadio"]').on('change', function() {
+		var i = $(this).val();
+		if (i == "1") {
+			$('.type1').hide();
+			$('.type2').show();
+		} else {
+			$('.type1').show();
+			$('.type2').hide();
+		}
+	});
+
+	DataManage.init();
+
+	//点击搜索
+	$('#searchuser').on('click', function() {
+		var oSettings = OdataTable.fnSettings();
+		oSettings._iDisplayStart = 0;
+		OdataTable.fnDraw();
+	});
+});
